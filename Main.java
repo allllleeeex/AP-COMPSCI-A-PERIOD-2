3a995150c7925cfbae94f147f005efe8116cdd06
allllleeeex <agepp27@gmail.com>
2024-09-13 09:25:04 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,11 +2,12 @@ public class Main
 {
   public static void main(String[] args)
   {
+  final String v = "Lalala";
   double r = 6;
   int e = 7;
   boolean c = false;
   e = e - 4;
-  System.out.println (c);
+  System.out.println (c + v);
   System.out.print (r + e);
   }
 }
